e59e5bf868d66cbbce102d111e02fa853efb450b
Tolea01 <anatoliecurbat@gmail.com>
2025-09-16 23:06:28 +0300

    create & config logger

---
+++ src/utils/logger.ts
@@ -0,0 +1,22 @@
+import winston from 'winston';
+import { ConfigService } from '../config/config.service';
+
+const LOGGER: winston.Logger = winston.createLogger({
+  level: new ConfigService().get('LOG_LEVEL'),
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json(),
+  ),
+  transports: [
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' }),
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple(),
+      ),
+    }),
+  ],
+});
+
+export default LOGGER;
