6823e1de7c1ca89c9bee9cf664e21b5d49ebabd8
Tolea01 <anatoliecurbat@gmail.com>
2025-09-16 22:40:47 +0300

    add & config pretter

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,31 +1,31 @@
-import type { DotenvParseOutput } from 'dotenv'
-import { config } from 'dotenv'
-import type { IConfigService } from './config.interface'
+import type { DotenvParseOutput } from 'dotenv';
+import { config } from 'dotenv';
+import type { IConfigService } from './config.interface';
 
 export class ConfigService implements IConfigService {
-	private config: DotenvParseOutput
+  private config: DotenvParseOutput;
 
-	constructor() {
-		const { error, parsed } = config()
+  constructor() {
+    const { error, parsed } = config();
 
-		if (error) {
-			throw new Error('The .env file was not found')
-		}
+    if (error) {
+      throw new Error('The .env file was not found');
+    }
 
-		if (!parsed) {
-			throw new Error('The .env file is empty')
-		}
+    if (!parsed) {
+      throw new Error('The .env file is empty');
+    }
 
-		this.config = parsed
-	}
+    this.config = parsed;
+  }
 
-	get(key: string): string {
-		const res: string | undefined = this.config[key]
+  get(key: string): string {
+    const res: string | undefined = this.config[key];
 
-		if (!res) {
-			throw new Error('No such key')
-		}
+    if (!res) {
+      throw new Error('No such key');
+    }
 
-		return res
-	}
+    return res;
+  }
 }
