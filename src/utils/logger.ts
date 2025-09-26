c76e0b5862abf723ba75e4e50a0dc17d6adf26a5
Tolea01 <anatoliecurbat@gmail.com>
2025-09-26 11:32:37 +0300

    code review

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,4 +1,4 @@
-import winston from 'winston';
+import * as winston from 'winston';
 import { ConfigService } from '../config/config.service';
 
 const LOGGER: winston.Logger = winston.createLogger({
